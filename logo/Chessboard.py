abfb822ea1a0e3464cc92157665880b60ceb8d68
Minghsien <martin.humphries1@gmail.com>
2015-08-18 12:16:59 +0100

    Running logo/Chessboard.py

--- logo/Chessboard.py
+++ logo/Chessboard.py
@@ -1,9 +1,9 @@
 from tealight.logo import move, turn
 def Chessboard(size):
-  for i in range(4):
-    move(size)
-    turn(90)
-  turn(90)
+  #for i in range(4):
+    #move(size)
+    #turn(90)
+  #turn(90)
   for i in range(4):
     move(size/8)
     turn(-90)
