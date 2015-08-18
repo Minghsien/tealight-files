5ec654a359f7a1b150314a83515c8c7e6a21227b
Minghsien <martin.humphries1@gmail.com>
2015-08-18 12:21:31 +0100

    Running logo/Chessboard.py

--- logo/Chessboard.py
+++ logo/Chessboard.py
@@ -1,27 +1,29 @@
 from tealight.logo import move, turn
+
+
+def drawsquare(size):
+  for i in range(4):
+    move(size)
+    turn(90)
+
+def drawlines(size):
+  for i in range(4):
+    move(size/8)
+    turn(-90)
+    move(size)
+    turn(90)
+    move(size/8)
+    turn(90)
+    move(size)
+    turn(-90)
+  
 def Chessboard(size):
-  for i in range(4):
-    move(size)
-    turn(90)
+  drawsquare(size)
   turn(90)
-  for i in range(4):
-    move(size/8)
-    turn(-90)
-    move(size)
-    turn(90)
-    move(size/8)
-    turn(90)
-    move(size)
-    turn(-90)
+  drawlines(size)
   turn(-90)
-  for i in range(4):
-    move(size/8)
-    turn(-90)
-    move(size)
-    turn(90)
-    move(size/8)
-    turn(90)
-    move(size)
-    turn(-90)
+  drawlines(size)
+
 Chessboard(128)
+  
     
