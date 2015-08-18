f20c025dd18f3e732439dd42ca342d9bf3755ef6
Minghsien <martin.humphries1@gmail.com>
2015-08-18 15:01:47 +0100

    Running robot/trail.py

--- robot/trail.py
+++ robot/trail.py
@@ -7,8 +7,11 @@ from tealight.robot import (move,
                             right_side)
 
 while True:
-  move()
-  if right_side() == "fruit":
+  if look() == "fruit":
+    move()
+  elif right_side() == "fruit":
     turn(1)
   elif left_side() == "fruit":
     turn(-1)
+  else:
+    turn(1)
