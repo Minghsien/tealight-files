c2f8e1c82888a1e5c6a5d1748309552b791493d5
Minghsien <martin.humphries1@gmail.com>
2015-08-20 15:10:04 +0100

    Running art/rotation.py

--- art/rotation.py
+++ art/rotation.py
@@ -47,6 +47,7 @@ def handle_keydown(key):
     newcar = rotation(newcar,-10)
     drawcar(newcar,"blue")
   elif key == "right":
+    box(0,0,screen_width,screen_height)
     drawcar(newcar,"white")
       
     newcar = rotation(newcar,10)
