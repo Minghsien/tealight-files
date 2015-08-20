5b6b81e99edb9e8199184978f717d3ad966a7cf1
Minghsien <martin.humphries1@gmail.com>
2015-08-20 13:54:44 +0100

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -12,7 +12,7 @@ car = [(x,y),(x+50,y),(x-40,y-25),(x-40,y+25),(x-40,y-25)
        (x-40,y-25),(x+50,y)]
 
 def rotation(car, theta):
-   theta = math.radians(theta)
+   theta = radians(theta)
    newcar = []
    for vertex in car:
       newcar.append((vertex[0]*cos(theta)-vertex[1]*sin(theta),vertex[0]*sin(theta)+vertex[1]*cos(theta)))
