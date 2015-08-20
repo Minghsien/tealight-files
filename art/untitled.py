55dda794b5b680f1c6ffa1a1f4e0d14e48173c5f
Minghsien <martin.humphries1@gmail.com>
2015-08-20 13:52:44 +0100

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -4,7 +4,7 @@ carcolour = "red"
 y = screen_height/2
 x = screen_width/2
 
-counter = 0 
+
 
 
 car = [(x,y),(x+50,y),(x-40,y-25),(x-40,y+25),(x-40,y-25)
@@ -19,6 +19,7 @@ def rotation(car, theta):
 
 def drawcar(car):
   temppoints = []
+  counter = 0 
   for each in car:
     temppoints.append(each[0])
     temppoints.append(each[1])
