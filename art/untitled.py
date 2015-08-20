be08f1323fbea1a6bbf6d9b665d10cb594d7d7a6
Minghsien <martin.humphries1@gmail.com>
2015-08-20 14:23:41 +0100

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -15,7 +15,7 @@ def rotation(shape, theta):
    newshape = []
    for vertex in shape:
       print(vertex[0],vertex[1])
-      newshape.append((((vertex[0]-x)*cos(theta)-(vertex[1]-y)*sin(theta))+x,(vertex[0]*sin(theta)+vertex[1]*cos(theta)+y)))
+      newshape.append((((vertex[0]-x)*cos(theta)-(vertex[1]-y)*sin(theta))+x,((vertex[0]-x)*sin(theta)+(vertex[1]-y)*cos(theta)+y)))
    newcar = newshape
    print(newcar)
    return newcar
