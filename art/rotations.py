846d42ae29b5c5bf40ef9f99345bbc8f63bf8dea
Minghsien <martin.humphries1@gmail.com>
2015-08-20 12:14:23 +0100

    Closing art/rotations.py

--- art/rotations.py
+++ art/rotations.py
@@ -1,1 +1,10 @@
-print "A new file!"
+colour = "red"
+theta = 90
+
+
+def rotatedCar(x,y,colour,theta):
+  color(colour)
+  line(x,y,cos(theta)*x+50-sin(theta)*y,x*sin(theta)+y*cos(theta))
+  line(cos(theta)*(x-40)-sin(theta)*(y-25),(x-40)*sin(theta)+(y-25)*cos(theta),(x-40)*cos(theta)-(y-25)*sin(theta),(x-40)*sin(theta)+(y+25)*cos(theta))
+  
+rotatedcar(carcentrex,carcentrey,colour,theta)
