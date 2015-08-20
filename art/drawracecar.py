7d487bbbe2911aa6078b6d427bf9386779b429b9
Minghsien <martin.humphries1@gmail.com>
2015-08-20 10:37:21 +0100

    Running art/drawracecar.py

--- art/drawracecar.py
+++ art/drawracecar.py
@@ -3,6 +3,7 @@ from tealight.art import *
 carcentrey = screen_height/2
 carcentrex = screen_width/2
 def DrawCar(x,y):
+  color("blue")
   line(x,y,x+50,y)
   line(x-40,y-25,x-40,y+25)
   line(x-40,y-25,x,y)
