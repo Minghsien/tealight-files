8fb18118c5d4ad6d7963145c8d9a9fcd701dfc2c
Minghsien <martin.humphries1@gmail.com>
2015-08-20 10:26:34 +0100

    Running art/drawracecar.py

--- art/drawracecar.py
+++ art/drawracecar.py
@@ -3,4 +3,4 @@ from tealight.art import *
 carcentrey = screen_height/2
 carcentrex = screen_width/2
 line(carcentrex,carcentrey,carcentrex+50,carcentrey)
-line(carcentrex-50,carcentrey-25,carcentrex-50,carcentrey+25)
+line(carcentrex-40,carcentrey-25,carcentrex-50,carcentrey+25)
