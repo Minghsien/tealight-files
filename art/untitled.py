9a0f7c0c37bd17a98ed870e08160753d3a2948d7
Minghsien <martin.humphries1@gmail.com>
2015-08-20 14:23:54 +0100

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -31,7 +31,7 @@ def drawcar(points):
       line(temppoints[0],temppoints[1],temppoints[2],temppoints[3])
       temppoints =[]
       counter = 0
-newcar = rotation(car,0)
+newcar = rotation(car,45)
 drawcar(newcar)
       
       
