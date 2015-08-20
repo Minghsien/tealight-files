8b15dfc33e3d60b1748b072890389198efc3fd55
Minghsien <martin.humphries1@gmail.com>
2015-08-20 10:27:56 +0100

    Running art/drawracecar.py

--- art/drawracecar.py
+++ art/drawracecar.py
@@ -4,3 +4,4 @@ carcentrey = screen_height/2
 carcentrex = screen_width/2
 line(carcentrex,carcentrey,carcentrex+50,carcentrey)
 line(carcentrex-40,carcentrey-25,carcentrex-40,carcentrey+25)
+line(carcentrex-40,carcentrey-25,carcentrex,carcentrey)
