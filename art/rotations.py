a1af4f306c0bea54e79b089e297b50be173ae913
Minghsien <martin.humphries1@gmail.com>
2015-08-20 12:16:21 +0100

    Running art/rotations.py

--- art/rotations.py
+++ art/rotations.py
@@ -2,7 +2,7 @@ from tealight.art import *
 from math import sin,cos
 
 colour = "red"
-theta = 90
+theta = 180
 carcentrey = screen_height/2
 carcentrex = screen_width/2
 
@@ -14,3 +14,7 @@ def rotatedCar(x,y,colour,theta):
   line(cos(theta)*(x-40)-sin(theta)*(y-25),(x-40)*sin(theta)+(y-25)*cos(theta),(x-40)*cos(theta)-(y-25)*sin(theta),(x-40)*sin(theta)+(y+25)*cos(theta))
   
 rotatedCar(carcentrex,carcentrey,colour,theta)
+
+
+def rotation(x,y):
+  
