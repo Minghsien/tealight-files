855f2d76710b161fc08181868fbd2e2fa02d2efa
Minghsien <martin.humphries1@gmail.com>
2015-08-20 11:50:53 +0100

    Closing art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -1,3 +1,4 @@
 from tealight.art import *
+color("red")
 vertice1 =[40,25,40,25]
-line(vertice1[0],vertice1[1],vertice1[2],vertice1[3],"red")
+line(vertice1[0],vertice1[1],vertice1[2],vertice1[3],)
