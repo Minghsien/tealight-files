e401fd4e21bc675ec9fd5513f7d625a613ee5722
Minghsien <martin.humphries1@gmail.com>
2015-08-20 15:07:48 +0100

    Running art/rotation.py

--- art/rotation.py
+++ art/rotation.py
@@ -39,7 +39,7 @@ drawcar(newcar,"blue")
 def handle_keydown(key):
   global newcar
   if key == "left":
-    background("white.png")
+    background("paper.jpg")
       
     drawcar(newcar,"white")
       
