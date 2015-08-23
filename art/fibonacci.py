ea1afb110100611c78b4637e2db3a3d51a51842c
Minghsien <martin.humphries1@gmail.com>
2015-08-23 12:46:03 +0100

    Running art/fibonacci.py

--- art/fibonacci.py
+++ art/fibonacci.py
@@ -1,11 +1,14 @@
 def fibsum():
   add = 0
   old = 1
+  temp = 0
   current = 1
   while current < 4000000:
     add += current
-    
+    temp = current
     current += old
+    old += temp
+    
   print add
        
 fibsum()  
