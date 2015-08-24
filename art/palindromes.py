be80b85dc52aca8c077c53ce46740541b72d1443
Minghsien <martin.humphries1@gmail.com>
2015-08-24 11:37:13 +0100

    Running art/palindromes.py

--- art/palindromes.py
+++ art/palindromes.py
@@ -2,8 +2,11 @@ def palindromes():
   for i in range(100,999):
     for j in range(100,999):
       num = str(i*j)
+      top = 0
       if num == num[::-1]:
+        if num > top:
+          top = num
       
-        print(num)
+  print(top)
 
 palindromes()
