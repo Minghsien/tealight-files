08d91c48e79c37b5dd768b1a16698b19497213da
Minghsien <martin.humphries1@gmail.com>
2015-08-24 12:47:07 +0100

    Running art/palindromes.py

--- art/palindromes.py
+++ art/palindromes.py
@@ -2,7 +2,7 @@ def palindromes():
   top = 0
   for i in range(10,99):
     for j in range(10,99):
-      num = strj)
+      num = str(i*j)
       print(num)
       if num == num[::-1]:
         if num > top:
