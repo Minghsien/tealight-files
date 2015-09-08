370a32f5d2cd3a714e2f0173ef369c5a6eabf5f4
Minghsien <martin.humphries1@gmail.com>
2015-09-08 09:19:50 +0100

    Running robot/cleanup.py

--- robot/cleanup.py
+++ robot/cleanup.py
@@ -6,7 +6,6 @@ from tealight.robot import (move,
                             left_side, 
                             right_side)
 while True:
-  move()
   if touch() == "fruit":
     move()
   else:
