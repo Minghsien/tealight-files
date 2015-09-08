17c7a6ba04eea53d45b020a5c0a30762c5f8baee
Minghsien <martin.humphries1@gmail.com>
2015-09-08 10:31:16 +0100

    Running art/divisible.py

--- art/divisible.py
+++ art/divisible.py
@@ -6,10 +6,5 @@ def isdivisable(num,n):
       m+=1
   if m == n:
     return True
-  
-num = 1
-while isdivisable(num, 20) == False:
-  num += 1
-  print(num)
-  
-print(num)
+
+isdivisable(2520,20)
