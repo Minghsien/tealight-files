88555c741f797f32a5687e572ed4da75b79d80cd
Minghsien <martin.humphries1@gmail.com>
2015-09-08 10:47:02 +0100

    Closing art/primes.py

--- art/primes.py
+++ art/primes.py
@@ -1,1 +1,14 @@
-print "A new file!"
+primes = []
+
+
+
+def isprime(num):
+  if num <=1:
+    return False
+  elif num <=3:
+    return True
+  else:
+    for i in range(num+1):
+      num %
+    
+  
