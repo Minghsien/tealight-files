2f39b0cb1007a7f0707efcc3f2409be9c1e18ad8
Minghsien <martin.humphries1@gmail.com>
2015-09-20 18:16:07 +0100

    Running art/pythagtriples.py

--- art/pythagtriples.py
+++ art/pythagtriples.py
@@ -15,15 +15,14 @@ triples = triples()
 
 
 def triplecheck(triples):
-  for i in range(len(triples)):
-    if i % 3 == 0:
-      if math.sqrt(triples[i]) + math.sqrt(triples[i-1]) + math.sqrt(triples[i-2]) == 1000:
-        print(math.sqrt(triples[i-2]))
-        print(math.sqrt(triples[i-1]))
-        print(math.sqrt(triples[i]))
+  for each in triples:
+    if math.sqrt(each[0]) + math.sqrt(each[1]) + math.sqrt(each[2]) == 1000:
+      print(math.sqrt(each[2]))
+      print(math.sqrt(each[1]))
+      print(math.sqrt(each[0]))
 
 
-#triplecheck(triples)  
+triplecheck(triples)  
   
 
   
