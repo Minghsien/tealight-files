a9a13b136ae7dc52593c6640071ce896cc540571
Minghsien <martin.humphries1@gmail.com>
2015-09-20 17:54:34 +0100

    Running art/pythagtriples.py

--- art/pythagtriples.py
+++ art/pythagtriples.py
@@ -1,1 +1,18 @@
-print "A new file!"
+squares = []
+for i in range(100):
+  squares.append(i**2)
+def triples():
+  triples = []
+  for i in range(len(squares)):
+    for each in squares:
+      if i+each in squares:
+        triples.append(i)
+        print(i)
+        triples.append(each)
+        print(each)
+        triples.append(i+each)
+        print(i+each)
+  print(triples)
+print(squares)
+triples()
+  
