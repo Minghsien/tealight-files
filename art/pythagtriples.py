570be84a64ed421a2144f04cb1b30bca36fd522f
Minghsien <martin.humphries1@gmail.com>
2015-09-20 17:56:32 +0100

    Running art/pythagtriples.py

--- art/pythagtriples.py
+++ art/pythagtriples.py
@@ -3,15 +3,15 @@ for i in range(1,100):
   squares.append(i**2)
 def triples():
   triples = []
-  for i in range(1,len(squares)):
-    for each in squares:
-      if i+each in squares:
+  for i in squares:
+    for j in squares:
+      if i+j in squares:
         triples.append(i)
         print(i)
-        triples.append(each)
-        print(each)
-        triples.append(i+each)
-        print(i+each)
+        triples.append(j)
+        print(j)
+        triples.append(i+j)
+        print(i+j)
   print(triples)
 print(squares)
 triples()
