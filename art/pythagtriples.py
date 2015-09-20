913bc76071ab31c3c03728c0808437fb8af68adf
Minghsien <martin.humphries1@gmail.com>
2015-09-20 18:21:03 +0100

    Running art/pythagtriples.py

--- art/pythagtriples.py
+++ art/pythagtriples.py
@@ -1,7 +1,7 @@
 import math
 
 squares = []
-for i in range(1,100):
+for i in range(1,1000):
   squares.append(i**2)
 def triples():
   triples = []
@@ -17,7 +17,7 @@ for each in triples:
   abc.append([math.sqrt(each[0]),math.sqrt(each[1]),math.sqrt(each[2])])
 print abc
 for each in abc:
-  if each[0]+each[1]+each[2] == 1000:
+  if (each[0]+each[1]+each[2]) == 1000:
     print each
 def triplecheck(triples):
   for each in triples:
