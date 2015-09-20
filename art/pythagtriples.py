7e9c869717e13ae5a27627588abd0e04060ee60d
Minghsien <martin.humphries1@gmail.com>
2015-09-20 18:04:56 +0100

    Running art/pythagtriples.py

--- art/pythagtriples.py
+++ art/pythagtriples.py
@@ -13,15 +13,16 @@ def triples():
         triples.append(i+j)
         print(i+j)
   print(triples)
-  return triples
-print(squares)
-triples = triples ()
-for i in range(len(triples)):
+  for i in range(len(triples)):
   if i % 3 == 0:
     if triples[i] + triples[i-1] + triples[i-2] == 1000:
       print(triples[i-2])
       print(triples[i-1])
       print(triples[i])
+
+
+
+
   
   
 
